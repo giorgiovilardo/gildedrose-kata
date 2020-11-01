e41a97d0bcf9d0315a4affef717357292288ad3d
Giorgio Vilardo <giorgio.vilardo@it.clara.net>
2020-11-01 21:19:44 +0100

    first ver

---
+++ src/gildedrose_kata/gildedrose.py
@@ -0,0 +1,62 @@
+from typing import List
+
+
+class Item:
+    def __init__(self, name: str, sell_in: int, quality: int):
+        self.name = name
+        self.sell_in = sell_in
+        self.quality = quality
+
+    def __repr__(self):
+        return f"{self.name}, {self.sell_in}, {self.quality}"
+
+
+class GildedRose:
+    def __init__(self, items: List[Item]):
+        self.items = items
+
+    def update_quality(self):
+        for item in self.items:
+            quality_control_function = self._determine_quality_func(item)
+            quality_control_function(item)  # noqa
+            item.sell_in -= 1
+
+    def _legendary(self, item: Item):
+        pass
+
+    def _aged_brie(self, item: Item):  # noqa
+        item.quality = item.quality + 1 if item.quality <= 49 else 50
+
+    def _conjured(self, item: Item):  # noqa
+        quantity_drop = 2 if item.sell_in > 0 else 4
+        item.quality = (
+            item.quality - quantity_drop if item.quality >= quantity_drop else 0
+        )
+
+    def _backstage(self, item: Item):  # noqa
+        if item.sell_in > 10:
+            item.quality += 1
+        elif 5 < item.sell_in <= 10:
+            item.quality += 2
+        elif 0 <= item.sell_in <= 5:
+            item.quality += 3
+        else:
+            item.quality = 0
+
+    def _normal(self, item: Item):  # noqa
+        quantity_drop = 1 if item.sell_in > 0 else 2
+        item.quality = (
+            item.quality - quantity_drop if item.quality >= quantity_drop else 0
+        )
+
+    def _determine_quality_func(self, item: Item):
+        special_item_funcs = {
+            "Sulfuras": self._legendary,
+            "Aged Brie": self._aged_brie,
+            "Conjured": self._conjured,
+            "Backstage": self._backstage,
+        }
+        for key in special_item_funcs.keys():
+            if key in item.name:
+                return special_item_funcs[key]
+        return self._normal
---
+++ tests/test_gildedrose_kata.py
@@ -0,0 +1,90 @@
+import pytest
+from gildedrose_kata import __version__
+from gildedrose_kata.gildedrose import Item, GildedRose
+
+
+def instance_gilded_rose(item_list, days_to_pass=1):
+    gilded_rose = GildedRose(item_list)
+    for i in range(days_to_pass):
+        gilded_rose.update_quality()
+    return gilded_rose
+
+
+def test_quality_dont_go_under_zero():
+    gr = instance_gilded_rose(
+        [Item("foo", 0, 0), Item(name="Conjured Mana Cake", sell_in=3, quality=2)], 2
+    )
+    assert gr.items[0].quality == 0
+    assert gr.items[1].quality == 0
+
+
+def test_brie_increasing():
+    gr = instance_gilded_rose([Item("Aged Brie", 0, 3)], 2)
+    assert gr.items[0].quality == 5
+
+
+def test_quality_dont_go_over_50():
+    gr = instance_gilded_rose([Item("Aged Brie", 10, 50)])
+    assert gr.items[0].quality == 50
+
+
+def test_legendary_items_stays_the_same():
+    gr = instance_gilded_rose(
+        [Item(name="Sulfuras, Hand of Ragnaros", sell_in=-1, quality=80)], 2
+    )
+    assert gr.items[0].quality == 80
+    assert gr.items[0].sell_in == -3
+
+
+def test_conjured_items_degrade_at_double_rate():
+    gr = instance_gilded_rose([Item(name="Conjured Mana Cake", sell_in=3, quality=6)])
+    assert gr.items[0].quality == 4
+    assert gr.items[0].sell_in == 2
+
+
+def test_conjured_late_items_degrade_at_double_double_rate():
+    gr = instance_gilded_rose([Item(name="Conjured Mana Cake", sell_in=-1, quality=6)])
+    assert gr.items[0].quality == 2
+    assert gr.items[0].sell_in == -2
+
+
+def test_backstage_pass_increase_by_one_before_10_days():
+    gr = instance_gilded_rose([Item(name="Backstage passes", sell_in=40, quality=6)])
+    assert gr.items[0].quality == 7
+    assert gr.items[0].sell_in == 39
+
+
+def test_backstage_pass_increase_by_two_between_10_and_5_days():
+    gr = instance_gilded_rose([Item(name="Backstage passes", sell_in=9, quality=6)])
+    assert gr.items[0].quality == 8
+    assert gr.items[0].sell_in == 8
+
+
+def test_backstage_pass_increase_by_three_between_5_and_0_days():
+    gr = instance_gilded_rose([Item(name="Backstage passes", sell_in=2, quality=6)])
+    assert gr.items[0].quality == 9
+    assert gr.items[0].sell_in == 1
+
+
+def test_backstage_pass_is_0_after_concert():
+    gr = instance_gilded_rose([Item(name="Backstage passes", sell_in=-1, quality=6)])
+    assert gr.items[0].quality == 0
+    assert gr.items[0].sell_in == -2
+
+
+def test_normal_items_degrades_normally():
+    gr = instance_gilded_rose([Item(name="Foo", sell_in=23, quality=4)])
+    assert gr.items[0].quality == 3
+    assert gr.items[0].sell_in == 22
+
+
+def test_normal_items_out_of_time_degrades_doubly():
+    gr = instance_gilded_rose([Item(name="Foo", sell_in=-23, quality=4)])
+    assert gr.items[0].quality == 2
+    assert gr.items[0].sell_in == -24
+
+
+def test_normal_items_out_of_time_degrades_doubly_error_case():
+    gr = instance_gilded_rose([Item(name="Foo", sell_in=-23, quality=1)])
+    assert gr.items[0].quality == 0
+    assert gr.items[0].sell_in == -24
